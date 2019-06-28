315c01b5ec332f4ad57697769a0f733fcb9e83fe
your name（注册时填写的名字） <453515088@qq.com>
2019-06-28 13:59:27 +0800

    增加数据库读取用例

--- AutoTest2/src/main/java/com/tester/config/TestConfig.java
+++ AutoTest2/src/main/java/com/tester/config/TestConfig.java
@@ -10,14 +10,6 @@ public class TestConfig {
 
     //登陆接口uri
     public static String loginUrl;
-    //更新用户信息接口uri
-    public static String updateUserInfoUrl;
-    //获取用户列表接口uri
-    public static String getUserListUrl;
-    //获取用户信息接口uri
-    public static String getUserInfoUrl;
-    //添加用户信息接口
-    public static String addUserUrl;
     //添加品牌信息接口
     public static String addBrandUrl;
     //更新品牌信息接口uri
